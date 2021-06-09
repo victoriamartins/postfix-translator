e362b0349060e20c70492141a83ca236a6003b63
victoriamartins <martinsvictoriaa@gmail.com>
2021-06-09 10:16:02 -0300

    lexer started

--- Program.cs
+++ Program.cs
@@ -4,10 +4,46 @@ namespace postfix
 {
     class Program
     {
+        static int pos = -1;
+        static char currentChar;
+        static Token lexer(string input)
+        {
+            Token t = new Token();
+            while(true) 
+            {
+                currentChar = readChar(input);
+                if (currentChar != ' ' && currentChar != '\n') break;
+            }
+            if (Char.IsNumber(currentChar)) 
+            {
+                // TODO: to check if the next char is also a number 
+                // (if it is, concat them)
+                t.setTypeNumber();
+                t.setValue(currentChar);
+                Console.WriteLine(currentChar + " é numero");
+                return t;
+            }
+            t.setTypeSymbol();
+            t.setValue(currentChar);
+            Console.WriteLine(currentChar + " é simbolo");
+            return t;
+        }
+
+        static char readChar(string input) 
+        {
+            pos++;
+            if(pos < input.Length) {
+                return input[pos];
+            }
+            return ' ';
+        }
+
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
             Console.WriteLine(input);
+            Token next = new Token();
+            next = lexer(input);
         }
     }
 }
